6971c093b8bd5ec1e63030a916c7a83b6fa9b496
Ivan Gurov <guuuroov@gmail.com>
2023-06-29 14:38:38 +0300

    4.1

--- final/4_1.java
+++ final/4_1.java
@@ -1,41 +1,142 @@
-import java.util.Random;
+import java.util.Scanner;
 
-public class app {
+class node {
 
-  public static void main(String[] args) {
-    printTree(treeBalanceTree(9), "");
-  }
+  node left, right;
+  int data;
 
-  static void printTree(Node root, String sp) {
-    if (root != null) {
-      System.out.println(sp + root.value);
-      printTree(root.left, sp + " ");
-      printTree(root.right, sp + " ");
-    }
-  }
+  // красный ==> true, черный ==> false
+  boolean color;
 
-  static Node treeBalanceTree(int n) {
-    Node node = null;
-    if (n == 0) {
-      return node;
-    } else {
-      int nl = n / 2;
-      int nr = n - nl - 1;
-      node = new Node(new Random().nextInt(100));
-      node.left = treeBalanceTree(nl);
-      node.right = treeBalanceTree(nr);
-    }
-    return node;
+  node(int data) {
+    this.data = data;
+    left = null;
+    right = null;
+
+// Новый узел, который создается, является всегда красного цвета.
+    color = true;
   }
 }
 
-class Node {
+public class LLRBTREE {
 
-  Node left;
-  Node right;
-  int value;
+  private static node root = null;
 
-  public Node(int value) {
-    this.value = value;
+  // Функция для поворота узла против часовой стрелки.
+  node rotateLeft(node myNode) {
+    System.out.printf("поворот влево!!\n");
+    node child = myNode.right;
+    node childLeft = child.left;
+
+    child.left = myNode;
+    myNode.right = childLeft;
+
+    return child;
   }
+
+  // Функция для поворота узла по часовой стрелке.
+  node rotateRight(node myNode) {
+    System.out.printf("вращение вправо\n");
+    node child = myNode.left;
+    node childRight = child.right;
+
+    child.right = myNode;
+    myNode.left = childRight;
+
+    return child;
+  }
+
+  // Функция для проверки того, является ли узел красного цвета или нет.
+  boolean isRed(node myNode) {
+    if (myNode == null) {
+      return false;
+    }
+    return (myNode.color == true);
+  }
+
+  // Функция для изменения цвета двух узлы.
+  void swapColors(node node1, node node2) {
+    boolean temp = node1.color;
+    node1.color = node2.color;
+    node2.color = temp;
+  }
+
+  // вставка в левостороннее Красно-черное дерево.
+  node insert(node myNode, int data) {
+// Обычный код вставки для любого двоичного файла
+    if (myNode == null) {
+      return new node(data);
+    }
+
+    if (data < myNode.data) {
+      myNode.left = insert(myNode.left, data);
+    } else if (data > myNode.data) {
+      myNode.right = insert(myNode.right, data);
+    } else {
+      return myNode;
+    }
+
+// случай 1.
+    // когда правый дочерний элемент красный, а левый дочерний элемент черный или не существует.
+    if (isRed(myNode.right) && !isRed(myNode.left)) {
+// Повернуть узел  влево
+      myNode = rotateLeft(myNode);
+
+// Поменять местами цвета дочернего узла всегда должен быть красным
+      swapColors(myNode, myNode.left);
+    }
+
+// случай 2
+    // когда левый ребенок, а также левый внук выделены красным цветом
+    if (isRed(myNode.left) && isRed(myNode.left.left)) {
+// Повернуть узел в право
+      myNode = rotateRight(myNode);
+      swapColors(myNode, myNode.right);
+    }
+
+// случай 3
+    // когда и левый, и правый дочерние элементы окрашены в красный цвет.
+    if (isRed(myNode.left) && isRed(myNode.right)) {
+// Инвертировать цвет узла это левый и правый дети.
+      myNode.color = !myNode.color;
+
+// Изменить цвет на черный.
+      myNode.left.color = false;
+      myNode.right.color = false;
+    }
+
+    return myNode;
+  }
+
+  // Обход по порядку
+  void inorder(node node) {
+    if (node != null)
+    {
+      inorder(node.left);
+      char c = '●';
+      if (node.color == false)
+        c = '◯';
+      System.out.print(node.data + ""+c+" ");
+      inorder(node.right);
+    }
+  }
+
+  public static void main(String[] args) {
+
+    LLRBTREE node = new LLRBTREE();
+    Scanner scan = new Scanner(System.in);
+
+    char ch;
+    do {
+      System.out.println("Введите целое число");
+
+      int num = scan.nextInt();
+      root = node.insert(root, num);
+
+      node.inorder(root);
+      System.out.println("\nВы хотите продолжить? (введите y или n)");
+      ch = scan.next().charAt(0);
+    } while (ch == 'Y' || ch == 'y');
+  }
+}
 }
